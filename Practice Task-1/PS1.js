e8fe7b121e0400992159f707136fe85b94c1c8d1
janizeel99 <janizil800@gmail.com>
2025-12-25 17:05:28 +0530

    Add files via upload

---
+++ Practice Task-1/PS1.js
@@ -0,0 +1,9 @@
+const Product = {
+    Title : "Cello Ball Pen",
+    Rating : 4.5,
+    Offer: "1% off",
+    Price : 20
+}
+
+
+document.getElementById("Prod").innerHTML = "Product Title = " + Product.Title + "<br>" + "Product Rating = " + Product.Rating + "<br>" + "Product Offer = " + Product.Offer + "<br>" + "Product Price = " + Product.Price;
