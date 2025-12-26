ae936b87c24149a476ddeae57ad82741d0160575
janizeel99 <janizil800@gmail.com>
2025-12-26 16:34:22 +0530

    Add files via upload

---
+++ Practice Task-4/PS4.js
@@ -0,0 +1,5 @@
+let fullName = prompt("Enter Your Name Without Spaces For Create New Account:");
+console.log("Welcome " + fullName + " To Our Website");
+
+let userName = "@" + fullName + fullName.length;
+console.log("Your UserName For Login is : " + userName + " For Our Website");
